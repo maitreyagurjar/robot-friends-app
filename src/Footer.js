04c0be25da96e8a191b929bf9314051da7bbb76d
maitreyagurjar <maitreya.gurjar@gmail.com>
2024-03-21 16:53:40 +0000

    Initial build

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer tc">
+            <p className="footer-text">Developed By: Maitreya Gurjar</p>
+            <p className="footer-text">During ZeroToMastery Course 2024</p>
+        </footer>
+    );
+}
+
+export default Footer;
